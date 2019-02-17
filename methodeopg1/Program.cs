46604b4f113b8c48726119720bedf32aa935150c
Emillarsen194 <47022984+Emillarsen194@users.noreply.github.com>
2019-02-17 14:36:12 +0100

    Add project files.

---
+++ methodeopg1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodeopg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("indtast dit første tal");
+            float numberOne = float.Parse(Console.ReadLine());
+            Console.WriteLine("indtast dit andet tal");
+            float numberTwo = float.Parse(Console.ReadLine());
+         
+            Console.WriteLine(numberOne + " gange med " + numberTwo + " = " + logic.Multiply(numberOne, numberTwo)); //multiply method
+            Console.WriteLine(numberOne + " divideret med " + numberTwo + " = " + logic.Divided(numberOne, numberTwo)); //division method
+            Console.WriteLine(numberOne + " opløftede med " + numberTwo + " = " + logic.Power(numberOne, numberTwo)); //powerof method
+
+            Console.ReadKey();
+                        
+        }
+       
+       
+    }
+}
---
+++ methodeopg1/logic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodeopg1
+{
+    class logic
+    {
+       
+        public static float Multiply(float numberOne, float numberTwo) // method that resturns a float where numberone and numbertwo is multiplied
+        {
+
+            float multiplyresult = numberOne * numberTwo;
+            return multiplyresult;
+        }
+
+        public static float Divided(float numberOne, float numberTwo) //method that returns a float with two numbers divivded 
+        {
+            float dividedResult = numberOne / numberTwo;
+            
+
+            return dividedResult;
+        }
+       
+        
+        public static double Power(double number1, double number2) //returns a number with the power of 
+        {
+
+            double powerOf = Math.Pow(number1, number2);
+            return powerOf;
+
+        }
+        
+        
+       
+        
+
+
+    }
+}
